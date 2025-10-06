167f6f18cf23bf34c30385c77ef34bda479db071
info7tik <info7tik@gmail.com>
2025-10-06 13:07:01 +0200

    rename the app to 'seed-and-pod'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.seedandpod.app',
-  appName: 'seed-and-pod-mobile',
-  webDir: 'dist/seed-and-pod-mobile/browser'
+  appName: 'seed-and-pod',
+  webDir: 'dist/seed-and-pod/browser'
 };
 
 export default config;
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, seed-and-pod-mobile');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, seed-and-pod');
   });
 });
