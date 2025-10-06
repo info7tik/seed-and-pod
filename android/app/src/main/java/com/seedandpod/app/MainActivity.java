ebd5e3638eed890f1ac12f3c7cc8c2236227c71c
info7tik <info7tik@gmail.com>
2025-10-06 12:59:15 +0200

    initial commit

---
+++ android/app/src/main/java/com/seedandpod/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.seedandpod.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.seedandpod.app',
+  appName: 'seed-and-pod-mobile',
+  webDir: 'dist/seed-and-pod-mobile/browser'
+};
+
+export default config;
