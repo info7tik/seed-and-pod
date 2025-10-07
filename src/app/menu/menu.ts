51535f0810632c936be18ddd5aae74f4c1e784b2
info7tik <info7tik@gmail.com>
2025-10-07 13:57:38 +0200

    menu with image

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -8,4 +8,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './menu.html',
   styleUrls: ['./menu.scss']
 })
-export class Menu {}
+export class Menu {
+  items = [
+    { title: 'Basket', subtitle: 'Manage your harvest basket', image: 'assets/menu/basket.png', route: '/basket' },
+    { title: 'Cultivation', subtitle: 'Plan and track cultivation', image: 'assets/menu/cultivation.png', route: '/cultivation' },
+    { title: 'Garden', subtitle: 'Organize your garden layout', image: 'assets/menu/garden.png', route: '/garden' },
+    { title: 'Stock', subtitle: 'View and update stock levels', image: 'assets/menu/stock.png', route: '/stock' }
+  ];
+}
