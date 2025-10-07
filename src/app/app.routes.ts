872f91ae9c01f1fee5608340e27abcc76f4d6098
info7tik <info7tik@gmail.com>
2025-10-07 13:39:37 +0200

    first menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Basket } from './basket/basket';
+import { Cultivation } from './cultivation/cultivation';
+import { Garden } from './garden/garden';
+import { Stock } from './stock/stock';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'basket', component: Basket },
+  { path: 'cultivation', component: Cultivation },
+  { path: 'garden', component: Garden },
+  { path: 'stock', component: Stock }
+];
---
+++ src/app/cultivation/cultivation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cultivation',
+  imports: [],
+  templateUrl: './cultivation.html',
+  styleUrl: './cultivation.scss'
+})
+export class Cultivation {
+
+}
