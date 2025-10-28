b8eb924ca6f58ec7f362a505ebeccb8b5ee71fa3
info7tik <info7tik@gmail.com>
2025-10-28 13:36:00 +0100

    add new empty component to create the harvest form

---
+++ src/app/new-harvest/new-harvest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-harvest',
+  imports: [],
+  templateUrl: './new-harvest.component.html',
+  styleUrl: './new-harvest.component.scss'
+})
+export class NewHarvestComponent {
+
+}
